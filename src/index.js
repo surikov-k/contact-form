620ce684427121231568254fde691d43af705a5a
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-10-23 16:54:50 +0300

    feat(server): Add a post route

--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ const port = 5050;
 const app = express();
 app.use(express.json());
 
+app.post('/send-message', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
+})
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
