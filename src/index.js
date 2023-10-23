c099f0ed86a74810a583a36dfbecfee34784cb97
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-10-23 22:24:43 +0300

    feat(app): Send data to sheet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { z, ZodError } from 'zod';
 
+import sheets, {SHEET_ID} from './sheet-client.js'
+
 const port = 5050;
 
 const app = express();
@@ -12,9 +14,19 @@ const contactFormSchema = z.object({
   message: z.string().min(1, { message: 'Message is required' }),
 })
 
-app.post('/send-message', (req, res) => {
+app.post('/send-message', async (req, res) => {
   try {
     const {name, email, message} = contactFormSchema.parse(req.body);
+    await sheets.spreadsheets.values.append({
+      spreadsheetId: SHEET_ID,
+      range: 'Data!A2:C2',
+      insertDataOption: 'INSERT_ROWS',
+      valueInputOption: 'RAW',
+      requestBody: {
+        values: [[name, email, message]]
+      }
+    });
+    res.json({message: 'Data added successfully!'});
   } catch (e) {
     if (e instanceof ZodError) {
       res.status(400).json({error: e.message});
--- src/sheet-client.js
+++ src/sheet-client.js
@@ -1,10 +1,10 @@
 import {google} from "googleapis";
-import {client_email, private_key} from './secrets.json' assert {type: 'json'};
+import keys from './secrets.json' assert {type: 'json'};
 
 export const SHEET_ID = '1S2XxOr8C2Ep1OR9TAYgti1PuPAW1z1JLEgqt3k4-uxk';
 
 const client = new google.auth.JWT(
-  client_email,null, private_key, ['https://www.googleapis.com/auth/spreadsheets']
+  keys.client_email,null, keys.private_key, ['https://www.googleapis.com/auth/spreadsheets']
 )
 
 const sheets = google.sheets({
