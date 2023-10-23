a43ada842d5a9580bce8281fb990b635f46152a7
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-10-23 22:00:16 +0300

    feat(server): Add post schema validation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import express from 'express';
+import { z, ZodError } from 'zod';
 
 const port = 5050;
 
 const app = express();
 app.use(express.json());
 
+const contactFormSchema = z.object({
+  name: z.string().min(1, { message: 'Name is required' }),
+  email: z.string().email(),
+  message: z.string().min(1, { message: 'Message is required' }),
+})
+
 app.post('/send-message', (req, res) => {
-  console.log(req.body);
-  res.send('OK');
+  try {
+    const {name, email, message} = contactFormSchema.parse(req.body);
+  } catch (e) {
+    if (e instanceof ZodError) {
+      res.status(400).json({error: e.message});
+    } else {
+      res.sendStatus(500);
+    }
+  }
+
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
